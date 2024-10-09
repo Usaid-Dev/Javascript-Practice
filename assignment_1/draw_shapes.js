b740eb5eb45641595aa94fa44a5e6416f3021bc1
Usaid Asif <usaidasif12@gmail.com>
2024-10-09 09:06:00 +0500

    completed assignment 1 ( draw shapes )

---
+++ assignment_1/draw_shapes.js
@@ -0,0 +1,76 @@
+console.log("");
+console.log("Square");
+console.log("");
+console.log("*****");
+console.log("*****");
+console.log("*****");
+console.log("*****");
+console.log("*****");
+console.log("");
+
+console.log("Right-angled Triangle");
+console.log("");
+console.log("*");
+console.log("**");
+console.log("***");
+console.log("****");
+console.log("*****");
+console.log("");
+
+console.log("Inverted Triangle");
+console.log("");
+console.log("*****");
+console.log("****");
+console.log("***");
+console.log("**");
+console.log("*");
+console.log("");
+
+console.log("Pyramid");
+console.log("");
+console.log("    *    ");
+console.log("   ***   ");
+console.log("  *****  ");
+console.log(" ******* ");
+console.log("*********");
+console.log("");
+
+console.log("Diamond");
+console.log("");
+console.log("    *    ");
+console.log("   ***   ");
+console.log("  *****  ");
+console.log(" ******* ");
+console.log("*********");
+console.log(" ******* ");
+console.log("  *****  ");
+console.log("   ***   ");
+console.log("    *    ");
+console.log("");
+
+console.log("Hollow Square");
+console.log("");
+console.log("*****");
+console.log("*   *");
+console.log("*   *");
+console.log("*   *");
+console.log("*****");
+console.log("");
+
+console.log("Hollow Right-angled Triangle");
+console.log("");
+console.log("*");
+console.log("**");
+console.log("* *");
+console.log("*  *");
+console.log("*****");
+console.log("");
+
+console.log("Zigzag Pattern");
+console.log("");
+console.log("*   *   *");
+console.log(" * * * * ");
+console.log("  *   *  ");
+console.log(" * * * * ");
+console.log("*   *   *");
+console.log("");
